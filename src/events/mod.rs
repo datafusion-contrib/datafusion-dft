e75d744cd520e64341e4f02dea85793423f213cf
Matthew Turner <matthew.m.turner@outlook.com>
2022-03-30 23:48:30 -0400

    Refactor mods

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -15,10 +15,15 @@
 // specific language governing permissions and limitations
 // under the License.
 
-pub mod events;
 mod key;
 
 use crossterm::event::KeyEvent;
+
+pub use self::key::Key;
+
+use crossterm::event;
+use std::time::Duration;
+use std::{sync::mpsc, thread};
 
 pub enum InputEvent {
     /// Key event occured.
@@ -27,4 +32,41 @@ pub enum InputEvent {
     Tick,
 }
 
-pub use {self::key::Key, events::Event, events::Events};
+pub enum Event {
+    KeyInput(Key),
+    Tick,
+}
+
+pub struct Events {
+    rx: mpsc::Receiver<Event>,
+    // Need to be kept around to prevent disposing the sender side.
+    _tx: mpsc::Sender<Event>,
+}
+
+impl Events {
+    pub fn new(tick_rate: Duration) -> Events {
+        let (tx, rx) = mpsc::channel();
+
+        let event_tx = tx.clone(); // the thread::spawn own event_tx
+        thread::spawn(move || {
+            loop {
+                // poll for tick rate duration, if no event, sent tick event.
+                if crossterm::event::poll(tick_rate).unwrap() {
+                    if let event::Event::Key(key) = event::read().unwrap() {
+                        let key = Key::from(key);
+                        event_tx.send(Event::KeyInput(key)).unwrap();
+                    }
+                }
+                event_tx.send(Event::Tick).unwrap();
+            }
+        });
+
+        Events { rx, _tx: tx }
+    }
+
+    /// Attempts to read an event.
+    /// This function block the current thread.
+    pub fn next(&self) -> Result<Event, mpsc::RecvError> {
+        self.rx.recv()
+    }
+}
