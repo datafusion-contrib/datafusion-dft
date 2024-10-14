390bd4850fedb8d030928762c0767dfb30ee9ddb
Matthew Turner <matthew.m.turner@outlook.com>
2024-10-13 22:35:22 -0400

    Better output

--- src/execution/mod.rs
+++ src/execution/mod.rs
@@ -47,20 +47,22 @@ pub struct DurationsSummary {
     pub max: Duration,
     pub mean: Duration,
     pub median: Duration,
+    pub percent_of_total: f64,
 }
 
 impl std::fmt::Display for DurationsSummary {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "Min: {:?}", self.min)?;
         writeln!(f, "Max: {:?}", self.max)?;
-        writeln!(f, "Mean: {:?}", self.mean)?;
-        writeln!(f, "Median: {:?}", self.median)
+        writeln!(f, "Median: {:?}", self.median)?;
+        writeln!(f, "Mean: {:?} ({:.2}%)", self.mean, self.percent_of_total)
     }
 }
 
 /// Contains stats for all runs of a benchmarked query and provides methods for aggregating
 #[derive(Debug, Default)]
 pub struct BenchmarkStats {
+    query: String,
     runs: usize,
     logical_planning_durations: Vec<Duration>,
     physical_planning_durations: Vec<Duration>,
@@ -70,6 +72,7 @@ pub struct BenchmarkStats {
 
 impl BenchmarkStats {
     fn new(
+        query: String,
         logical_planning_durations: Vec<Duration>,
         physical_planning_durations: Vec<Duration>,
         execution_durations: Vec<Duration>,
@@ -77,6 +80,7 @@ impl BenchmarkStats {
     ) -> Self {
         let runs = logical_planning_durations.len();
         Self {
+            query,
             runs,
             logical_planning_durations,
             physical_planning_durations,
@@ -85,7 +89,7 @@ impl BenchmarkStats {
         }
     }
 
-    fn summarize(durations: &[Duration]) -> DurationsSummary {
+    fn summarize(&self, durations: &[Duration]) -> DurationsSummary {
         let mut sorted = durations.to_vec();
         sorted.sort();
         let len = sorted.len();
@@ -93,11 +97,21 @@ impl BenchmarkStats {
         let max = *sorted.last().unwrap();
         let mean = sorted.iter().sum::<Duration>() / len as u32;
         let median = sorted[len / 2];
+        let this_total = durations.iter().map(|d| d.as_nanos()).sum::<u128>();
+        let total_duration = self
+            .total_durations
+            .iter()
+            .map(|d| d.as_nanos())
+            .sum::<u128>();
+        let percent_of_total = (this_total as f64 / total_duration as f64) * 100.0;
+        // let percent_of_total =
+        //     (this_total.as_nanos() as f64 / total_duration.as_nanos() as f64) * 100.0;
         DurationsSummary {
             min,
             max,
             mean,
             median,
+            percent_of_total,
         }
     }
 }
@@ -108,21 +122,22 @@ impl std::fmt::Display for BenchmarkStats {
         writeln!(f, "----------------------------")?;
         writeln!(f, "Benchmark Stats ({} runs)", self.runs)?;
         writeln!(f, "----------------------------")?;
+        writeln!(f, "{}", self.query)?;
+        writeln!(f, "----------------------------")?;
 
-        let logical_planning_summary = BenchmarkStats::summarize(&self.logical_planning_durations);
+        let logical_planning_summary = self.summarize(&self.logical_planning_durations);
         writeln!(f, "Logical Planning")?;
         writeln!(f, "{}", logical_planning_summary)?;
 
-        let physical_planning_summary =
-            BenchmarkStats::summarize(&self.physical_planning_durations);
+        let physical_planning_summary = self.summarize(&self.physical_planning_durations);
         writeln!(f, "Physical Planning")?;
         writeln!(f, "{}", physical_planning_summary)?;
 
-        let execution_summary = BenchmarkStats::summarize(&self.execution_durations);
+        let execution_summary = self.summarize(&self.execution_durations);
         writeln!(f, "Execution")?;
         writeln!(f, "{}", execution_summary)?;
 
-        let total_summary = BenchmarkStats::summarize(&self.total_durations);
+        let total_summary = self.summarize(&self.total_durations);
         writeln!(f, "Total")?;
         writeln!(f, "{}", total_summary)
     }
@@ -378,31 +393,29 @@ impl ExecutionContext {
             let statements = DFParser::parse_sql_with_dialect(query, &dialect)?;
             if statements.len() == 1 {
                 for _ in 0..iterations {
-                    let start = std::time::Instant::now();
                     let statement = statements[0].clone();
-                    let logical_planning_start = std::time::Instant::now();
+                    let start = std::time::Instant::now();
                     let logical_plan = self
                         .session_ctx()
                         .state()
                         .statement_to_plan(statement)
                         .await?;
-                    let logical_planning_duration = logical_planning_start.elapsed();
-                    let physical_planning_start = std::time::Instant::now();
+                    let logical_planning_duration = start.elapsed();
                     let physical_plan = self
                         .session_ctx()
                         .state()
                         .create_physical_plan(&logical_plan)
                         .await?;
-                    let physical_planning_duration = physical_planning_start.elapsed();
+                    let physical_planning_duration = start.elapsed();
                     let task_ctx = self.session_ctx().task_ctx();
                     let mut stream = execute_stream(physical_plan, task_ctx)?;
-                    let execution_start = std::time::Instant::now();
                     while stream.next().await.is_some() {}
-                    let execution_duration = execution_start.elapsed();
+                    let execution_duration = start.elapsed();
                     let total_duration = start.elapsed();
                     logical_planning_durations.push(logical_planning_duration);
-                    physical_planning_durations.push(physical_planning_duration);
-                    execution_durations.push(execution_duration);
+                    physical_planning_durations
+                        .push(physical_planning_duration - logical_planning_duration);
+                    execution_durations.push(execution_duration - physical_planning_duration);
                     total_durations.push(total_duration);
                 }
             } else {
@@ -410,6 +423,7 @@ impl ExecutionContext {
             }
 
             Ok(BenchmarkStats::new(
+                query.to_string(),
                 logical_planning_durations,
                 physical_planning_durations,
                 execution_durations,
