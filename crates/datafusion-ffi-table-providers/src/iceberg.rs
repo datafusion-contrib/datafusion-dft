ce2b6da9999149583c0b2064aa6e6e5e3f09226a
Matthew Turner <matthew.m.turner@outlook.com>
2025-01-24 20:30:28 -0700

    Add some more

--- crates/datafusion-ffi-table-providers/src/iceberg.rs
+++ crates/datafusion-ffi-table-providers/src/iceberg.rs
@@ -14,3 +14,31 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+
+use abi_stable::{
+    declare_root_module_statics,
+    library::{LibraryError, RootModule},
+    package_version_strings,
+    sabi_types::VersionStrings,
+    StableAbi,
+};
+use datafusion_ffi::table_provider::FFI_TableProvider;
+
+#[repr(C)]
+#[derive(StableAbi)]
+#[sabi(kind(Prefix(prefix_ref = IcebergTableProviderModuleRef)))]
+pub struct IcebergTableProviderModule {
+    /// Constructs the table provider
+    pub create_table: extern "C" fn() -> FFI_TableProvider,
+}
+
+impl RootModule for IcebergTableProviderModuleRef {
+    declare_root_module_statics! {IcebergTableProviderModuleRef}
+    const BASE_NAME: &'static str = "ffi_iceberg_table_provider";
+    const NAME: &'static str = "ffi_iceberg_table_provider";
+    const VERSION_STRINGS: VersionStrings = package_version_strings!();
+
+    fn initialization(self) -> Result<Self, LibraryError> {
+        Ok(self)
+    }
+}
