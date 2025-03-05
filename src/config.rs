18982ae7775276aa038bc00c428cb2bd25a269f7
Matthew Turner <matthew.m.turner@outlook.com>
2025-03-05 10:00:22 -0500

    Clippy

--- src/config.rs
+++ src/config.rs
@@ -20,20 +20,12 @@
 use std::path::PathBuf;
 
 use datafusion_app::config::ExecutionConfig;
-#[cfg(feature = "udfs-wasm")]
-use datafusion_udfs_wasm::WasmInputDataType;
 use directories::{ProjectDirs, UserDirs};
 use lazy_static::lazy_static;
 use serde::Deserialize;
-#[cfg(feature = "udfs-wasm")]
-use std::collections::HashMap;
 
-#[cfg(feature = "s3")]
-use color_eyre::Result;
-// #[cfg(feature = "s3")]
-// use object_store::aws::{AmazonS3, AmazonS3Builder};
 #[cfg(feature = "flightsql")]
-use datafusion_app::config::{AuthConfig, BasicAuth};
+use datafusion_app::config::AuthConfig;
 
 lazy_static! {
     pub static ref PROJECT_NAME: String = env!("CARGO_CRATE_NAME").to_uppercase().to_string();
@@ -117,191 +109,9 @@ impl Default for DisplayConfig {
     }
 }
 
-// #[cfg(feature = "s3")]
-// #[derive(Clone, Debug, Deserialize)]
-// pub struct S3Config {
-//     bucket_name: String,
-//     object_store_url: Option<String>,
-//     aws_access_key_id: Option<String>,
-//     aws_secret_access_key: Option<String>,
-//     _aws_default_region: Option<String>,
-//     aws_endpoint: Option<String>,
-//     aws_session_token: Option<String>,
-//     aws_allow_http: Option<bool>,
-// }
-//
-// #[cfg(feature = "s3")]
-// impl S3Config {
-//     pub fn object_store_url(&self) -> &Option<String> {
-//         &self.object_store_url
-//     }
-// }
-//
-// #[cfg(feature = "s3")]
-// impl S3Config {
-//     pub fn to_object_store(&self) -> Result<AmazonS3> {
-//         let mut builder = AmazonS3Builder::new();
-//         builder = builder.with_bucket_name(&self.bucket_name);
-//         if let Some(access_key) = &self.aws_access_key_id {
-//             builder = builder.with_access_key_id(access_key)
-//         }
-//         if let Some(secret) = &self.aws_secret_access_key {
-//             builder = builder.with_secret_access_key(secret)
-//         }
-//         if let Some(endpoint) = &self.aws_endpoint {
-//             builder = builder.with_endpoint(endpoint);
-//         }
-//         if let Some(token) = &self.aws_session_token {
-//             builder = builder.with_token(token)
-//         }
-//         if let Some(allow_http) = &self.aws_allow_http {
-//             builder = builder.with_allow_http(*allow_http)
-//         }
-//
-//         Ok(builder.build()?)
-//     }
-// }
-
-// #[cfg(feature = "huggingface")]
-// #[derive(Clone, Debug, Deserialize)]
-// pub struct HuggingFaceConfig {
-//     pub repo_type: Option<String>,
-//     pub repo_id: Option<String>,
-//     pub revision: Option<String>,
-//     pub root: Option<String>,
-//     pub token: Option<String>,
-// }
-
-// #[derive(Clone, Debug, Deserialize)]
-// pub struct ObjectStoreConfig {
-//     #[cfg(feature = "s3")]
-//     pub s3: Option<Vec<S3Config>>,
-//     #[cfg(feature = "huggingface")]
-//     pub huggingface: Option<Vec<HuggingFaceConfig>>,
-// }
-
-// #[derive(Clone, Debug, Deserialize)]
-// pub struct ExecutionConfig {
-//     pub object_store: Option<ObjectStoreConfig>,
-//     #[serde(default = "default_ddl_path")]
-//     pub ddl_path: Option<PathBuf>,
-//     #[serde(default = "default_benchmark_iterations")]
-//     pub benchmark_iterations: usize,
-//     #[serde(default = "default_cli_batch_size")]
-//     pub cli_batch_size: usize,
-//     #[serde(default = "default_tui_batch_size")]
-//     pub tui_batch_size: usize,
-//     #[serde(default = "default_flightsql_server_batch_size")]
-//     pub flightsql_server_batch_size: usize,
-//     #[serde(default = "default_dedicated_executor_enabled")]
-//     pub dedicated_executor_enabled: bool,
-//     #[serde(default = "default_dedicated_executor_threads")]
-//     pub dedicated_executor_threads: usize,
-//     #[serde(default = "default_iceberg_config")]
-//     pub iceberg: IcebergConfig,
-//     #[cfg(feature = "udfs-wasm")]
-//     #[serde(default = "default_wasm_udf")]
-//     pub wasm_udf: WasmUdfConfig,
-// }
-
-// fn default_ddl_path() -> Option<PathBuf> {
-//     info!("Creating default ExecutionConfig");
-//     if let Some(user_dirs) = directories::UserDirs::new() {
-//         let ddl_path = user_dirs
-//             .home_dir()
-//             .join(".config")
-//             .join("dft")
-//             .join("ddl.sql");
-//         Some(ddl_path)
-//     } else {
-//         None
-//     }
-// }
-
 fn default_benchmark_iterations() -> usize {
     10
 }
-
-// fn default_cli_batch_size() -> usize {
-//     8092
-// }
-//
-// fn default_tui_batch_size() -> usize {
-//     100
-// }
-//
-// fn default_flightsql_server_batch_size() -> usize {
-//     8092
-// }
-
-// fn default_dedicated_executor_enabled() -> bool {
-//     false
-// }
-
-// fn default_dedicated_executor_threads() -> usize {
-//     // By default we slightly over provision CPUs.  For example, if you have N CPUs available we
-//     // have N CPUs for the [`DedicatedExecutor`] and 1 for the main / IO runtime.
-//     //
-//     // Ref: https://github.com/datafusion-contrib/datafusion-dft/pull/247#discussion_r1848270250
-//     num_cpus::get()
-// }
-
-// fn default_iceberg_config() -> IcebergConfig {
-//     IcebergConfig {
-//         rest_catalogs: Vec::new(),
-//     }
-// }
-//
-// #[cfg(feature = "udfs-wasm")]
-// fn default_wasm_udf() -> WasmUdfConfig {
-//     WasmUdfConfig {
-//         module_functions: HashMap::new(),
-//     }
-// }
-
-// impl Default for ExecutionConfig {
-//     fn default() -> Self {
-//         Self {
-//             object_store: None,
-//             ddl_path: default_ddl_path(),
-//             benchmark_iterations: default_benchmark_iterations(),
-//             cli_batch_size: default_cli_batch_size(),
-//             tui_batch_size: default_tui_batch_size(),
-//             flightsql_server_batch_size: default_flightsql_server_batch_size(),
-//             dedicated_executor_enabled: default_dedicated_executor_enabled(),
-//             dedicated_executor_threads: default_dedicated_executor_threads(),
-//             iceberg: default_iceberg_config(),
-//             #[cfg(feature = "udfs-wasm")]
-//             wasm_udf: default_wasm_udf(),
-//         }
-//     }
-// }
-
-// #[derive(Clone, Debug, Deserialize)]
-// pub struct RestCatalogConfig {
-//     pub name: String,
-//     pub addr: String,
-// }
-//
-// #[derive(Clone, Debug, Deserialize)]
-// pub struct IcebergConfig {
-//     pub rest_catalogs: Vec<RestCatalogConfig>,
-// }
-
-// #[cfg(feature = "udfs-wasm")]
-// #[derive(Clone, Debug, Deserialize)]
-// pub struct WasmFuncDetails {
-//     pub name: String,
-//     pub input_types: Vec<String>,
-//     pub return_type: String,
-//     pub input_data_type: WasmInputDataType,
-// }
-//
-// #[cfg(feature = "udfs-wasm")]
-// #[derive(Clone, Debug, Deserialize)]
-// pub struct WasmUdfConfig {
-//     pub module_functions: HashMap<PathBuf, Vec<WasmFuncDetails>>,
-// }
 
 #[derive(Clone, Debug, Default, Deserialize)]
 pub struct InteractionConfig {
@@ -366,19 +176,3 @@ fn default_editor_config() -> EditorConfig {
 fn default_auth_config() -> AuthConfig {
     AuthConfig::default()
 }
-
-// #[cfg(feature = "flightsql")]
-// #[derive(Clone, Debug, Default, Deserialize)]
-// pub struct AuthConfig {
-//     pub client_basic_auth: Option<BasicAuth>,
-//     pub client_bearer_token: Option<String>,
-//     pub server_basic_auth: Option<BasicAuth>,
-//     pub server_bearer_token: Option<String>,
-// }
-//
-// #[cfg(feature = "auth")]
-// #[derive(Clone, Debug, Default, Deserialize)]
-// pub struct BasicAuth {
-//     pub username: String,
-//     pub password: String,
-// }
