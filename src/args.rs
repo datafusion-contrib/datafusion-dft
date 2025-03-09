3c38f716f9635a49d9d57363abd9431021e64445
Matthew Turner <matthew.m.turner@outlook.com>
2025-03-09 11:00:00 -0400

    Remove http

--- src/args.rs
+++ src/args.rs
@@ -120,12 +120,6 @@ impl DftArgs {
 
 #[derive(Clone, Debug, Subcommand)]
 pub enum Command {
-    /// Start a HTTP server
-    ServeHttp {
-        #[clap(short, long)]
-        config: Option<String>,
-    },
-
     /// Start a FlightSQL server
     ServeFlightSql {
         #[clap(short, long)]
