63a6a637b3fcf8cbf12302cbcd32d595bb0884c3
Matthew Turner <matthew.m.turner@outlook.com>
2025-04-24 10:17:27 -0400

    Test for s3

--- tests/cli_cases/tpch.rs
+++ tests/cli_cases/tpch.rs
@@ -19,6 +19,7 @@
 
 use crate::config::TestConfigBuilder;
 use assert_cmd::Command;
+use object_store::{aws::AmazonS3Builder, path::Path, ObjectStore};
 
 #[test]
 fn test_custom_config() {
@@ -49,4 +50,56 @@ fn test_custom_config() {
         let metadata = data.metadata().unwrap();
         assert!(metadata.len() > 1000);
     })
+}
+
+#[tokio::test]
+async fn test_custom_config_with_s3() {
+    let mut config_builder = TestConfigBuilder::default();
+    config_builder.with_db_path("s3://tpch-db/db/");
+    let bucket = "tpch-db";
+    let endpoint = "http://localhost:4566";
+    let access_key = "LSIAQAAAAAAVNCBMPNSG";
+    let secret = "5555555555555555555555555555555555555555";
+    let allow_http = true;
+    config_builder.with_s3_object_store(
+        "cli",
+        "s3",
+        bucket,
+        "s3://tpch-db",
+        endpoint,
+        access_key,
+        secret,
+        allow_http,
+    );
+    let config = config_builder.build("my_config.toml");
+
+    Command::cargo_bin("dft")
+        .unwrap()
+        .arg("--config")
+        .arg(config.path)
+        .arg("generate-tpch")
+        .assert()
+        .success();
+
+    let s3 = AmazonS3Builder::new()
+        .with_bucket_name(bucket)
+        .with_endpoint(endpoint)
+        .with_access_key_id(access_key)
+        .with_secret_access_key(secret)
+        .with_allow_http(allow_http)
+        .build()
+        .unwrap();
+
+    let r = s3
+        .list_with_delimiter(Some(&Path::parse("db/tables/dft/tpch/").unwrap()))
+        .await
+        .unwrap();
+
+    let needed_dirs = [
+        "customer", "orders", "lineitem", "nation", "part", "partsupp", "region", "supplier",
+    ];
+
+    for prefix in r.common_prefixes {
+        assert!(needed_dirs.contains(&prefix.to_string().as_str()));
+    }
 }
